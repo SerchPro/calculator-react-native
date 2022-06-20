20e33638db56b724bde0d7c820ad0a459cedd94b
SerchPro <proyects_serch@hotmail.com>
2022-06-20 00:38:30 -0500

    use useRef

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Text, View } from 'react-native';
 import { Button } from '../components/Button';
 import { styles } from '../theme/appTheme';
+
+
+enum Operators {
+  add, substract, multiply, divide
+}
 
 export const CalculatorScreen = () => {
 
 
   const [ number, setNumber ] = useState('0');
-  const [ beforeNumber, setBeforeNumber] = useState('0')
+  const [ beforeNumber, setBeforeNumber] = useState('0');
+
+  const lastOperation = useRef<Operators>()
 
   const clean = () =>{
-    console.log("click")
-    setNumber('0')
+    setNumber('0');
+    setBeforeNumber('0');
   }
 
   const btnDelete = () =>{
@@ -40,8 +47,15 @@ export const CalculatorScreen = () => {
      *  setNumber('0');
      * }
      */
-    
+  }
 
+  const changeNumToPrevious = () =>{
+    if(number.endsWith('.')){
+      setBeforeNumber( number.slice(0,-1) )
+    } else{
+      setBeforeNumber(number)
+    }
+    setNumber( '0' );
   }
   
   const createNumber = ( textNumber: string) => {
@@ -80,9 +94,38 @@ export const CalculatorScreen = () => {
   }
 
 
+  //  add, substract, multiply, divide
+
+  const btnDivide = () =>{
+    changeNumToPrevious();
+    lastOperation.current = Operators.divide
+  };
+
+  const btnMultiply = () =>{
+    changeNumToPrevious();
+    lastOperation.current = Operators.multiply
+  }
+
+  const btnSubstract = () =>{
+    changeNumToPrevious();
+    lastOperation.current = Operators.substract
+  }
+
+  const btnAdd = () =>{
+    changeNumToPrevious();
+    lastOperation.current = Operators.add
+  }
+
+
   return (
     <View style = {styles.calContainer}>
-        <Text style = { styles.smallResult }> { beforeNumber } </Text>
+
+        {
+          ( beforeNumber !== '0' ) && (
+            <Text style = { styles.smallResult }> { beforeNumber } </Text>
+          )
+        }
+
         <Text 
             style = { styles.result }
             numberOfLines = {1}
@@ -97,28 +140,28 @@ export const CalculatorScreen = () => {
           <Button text="c" color='#9B9B9B' action = {clean} />
           <Button text="+/-" color='#9B9B9B' action = {positiveNegative}/>
           <Button text="del" color='#9B9B9B' action = {btnDelete}/>
-          <Button text="/" color='#FF9427' action = {clean}/>
+          <Button text="/" color='#FF9427' action = {btnDivide}/>
         </View>
 
         <View  style = {styles.row}>
           <Button text="7"  action = { createNumber } />
           <Button text="8"  action = { createNumber } />
           <Button text="9"  action = { createNumber } />
-          <Button text="x" color='#FF9427' action = {clean}/>
+          <Button text="x" color='#FF9427' action = {btnMultiply}/>
         </View>
 
         <View  style = {styles.row}>
           <Button text="4" action = { createNumber }  />
           <Button text="5" action = { createNumber } />
           <Button text="6" action = { createNumber }  />
-          <Button text="-" color='#FF9427' action = {clean}/>
+          <Button text="-" color='#FF9427' action = {btnSubstract}/>
         </View>
 
         <View  style = {styles.row}>
           <Button text="1" action = { createNumber }  />
           <Button text="2" action = { createNumber } />
           <Button text="3" action = { createNumber }  />
-          <Button text="+" color='#FF9427' action = {clean}/>
+          <Button text="+" color='#FF9427' action = {btnAdd}/>
         </View>
 
         <View  style = {styles.row}>
